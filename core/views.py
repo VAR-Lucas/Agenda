89dd17350098e2494646e00a56a4b185f62224fa
LucasCruz <Lucas2002cruz@gmail.com>
2021-12-30 00:20:26 -0400

    Commit

---
+++ core/views.py
@@ -0,0 +1,90 @@
+from django.http import request, response
+from django.shortcuts import redirect, render, HttpResponse
+from core.models import Evento
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from datetime import datetime, timedelta
+from django.http.response import Http404, JsonResponse
+from django.contrib.auth.models import User
+
+# Create your views here.
+def login_user(request):
+    return render(request, 'login.html')
+
+def logout_user(request):
+    logout(request)
+    return redirect('/')
+
+def submit_login(request):
+    # Validaão de usuario
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        usuario = authenticate(username=username, password=password)
+        if usuario is not None:
+            login(request, usuario)
+            return redirect('/')
+        else:
+            messages.error(request, "Usuario ou senha inválido")
+    return redirect('/')
+
+
+
+@login_required(login_url='/login/')
+def lista_eventos(request):
+    usuario = request.user
+    data_atual = datetime.now() - timedelta(hours=1)
+    # faz com que só apareça os eventos do ususario que os adicionou
+    evento = Evento.objects.filter(usuario = usuario)
+
+    dados = { 'eventos': evento}
+    return render(request, 'agenda.html', dados)
+
+
+@login_required(login_url='/login/')
+def evento(request):
+    id_evento = request.GET.get('id')
+    dados = {}
+    if id_evento:
+        dados['evento'] = Evento.objects.get(id=id_evento)
+    return render(request, 'evento.html', dados)
+
+
+@login_required(login_url='/login/')
+def submit_evento(request):
+    if request.POST:
+        titulo =request.POST.get('titulo')
+        data_evento = request.POST.get('data_evento')
+        local_evento = request.POST.get('local_evento')
+        descricao = request.POST.get('descricao')
+        usuario = request.user
+        id_evento = request.POST.get('id_evento')
+        if id_evento:
+            Evento.objects.filter(id=id_evento).update(titulo=titulo,
+                                                       data_evento=data_evento,
+                                                       local_evento=local_evento,
+                                                       descricao=descricao)
+        else:
+            Evento.objects.create(
+                titulo=titulo,
+                data_evento=data_evento,
+                local_evento=local_evento,
+                descricao=descricao,
+                usuario=usuario
+            )
+    return redirect('/')
+
+
+@login_required(login_url='/login/')
+def delete_evento(request,id_evento):
+    usuario = request.user
+    try:
+        evento = Evento.objects.get(id=id_evento)
+    except Exception:
+        raise Http404
+    if usuario == evento.usuario:
+        evento.delete()
+    else:
+        raise Http404()
+    return redirect('/')
